a7a8a61273d1a40f11d64823f6f1a72eb73d8259
Angelica Turla <turla_angelica@yahoo.com>
2023-02-26 00:05:28 -0500

    try zora api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['gateway.ipfs.io'],
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.xyz',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.mypinata.cloud',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.io',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
+const link = new HttpLink({
+  uri: 'https://api.zora.co/graphql',
+})
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
